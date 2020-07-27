bc803b759c7e722e1460839f0fb26d86547b2aa5
suudupa <suhas.udupa@mail.mcgill.ca>
2020-07-26 23:03:34 -0400

    Added new icon for FAB

--- app/src/main/java/com/suudupa/lacoupe/ui/MainActivity.java
+++ app/src/main/java/com/suudupa/lacoupe/ui/MainActivity.java
@@ -2,22 +2,37 @@ package com.suudupa.lacoupe.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.suudupa.lacoupe.R;
+import com.suudupa.lacoupe.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavController.OnDestinationChangedListener {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        navController.addOnDestinationChangedListener(this);
+    }
+
+    @Override
+    public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+        if (destination.getId() == R.id.navigation_standings) {
+            binding.newItemBtn.setImageResource(R.drawable.ic_new_player);
+        } else {
+            binding.newItemBtn.setImageResource(R.drawable.ic_new_game);
+        }
     }
 }

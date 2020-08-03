1d8ecc7c2eebb91bc3b19ea013fcda5c5b783a09
suudupa <suhas.udupa@mail.mcgill.ca>
2020-08-03 00:53:52 -0400

    Added datepicker

--- app/src/main/java/com/suudupa/lacoupe/fragment/AddNewGameFragment.kt
+++ app/src/main/java/com/suudupa/lacoupe/fragment/AddNewGameFragment.kt
@@ -1,5 +1,7 @@
 package com.suudupa.lacoupe.fragment
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +11,14 @@ import androidx.navigation.Navigation
 import com.suudupa.lacoupe.R
 import com.suudupa.lacoupe.databinding.FragmentAddNewGameBinding
 import com.suudupa.lacoupe.utility.Utils
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddNewGameFragment : Fragment(), View.OnClickListener {
+
+class AddNewGameFragment : Fragment() {
 
     private lateinit var binding: FragmentAddNewGameBinding
+    private val calendar = Calendar.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Utils.changeStatusBarColor(requireActivity(), R.color.colorPrimary)
@@ -26,10 +32,24 @@ class AddNewGameFragment : Fragment(), View.OnClickListener {
     }
 
     private fun initEvents() {
-        binding.doneBtn.setOnClickListener(this)
+        binding.dateEt.setOnClickListener {
+            DatePickerDialog(requireContext(), date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+        binding.doneBtn.setOnClickListener {
+            Navigation.findNavController(requireActivity().findViewById(R.id.nav_host_fragment)).popBackStack()
+        }
     }
 
-    override fun onClick(v: View?) {
-        Navigation.findNavController(requireActivity().findViewById(R.id.nav_host_fragment)).popBackStack()
+    private val date = OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+        calendar.set(Calendar.YEAR, year)
+        calendar.set(Calendar.MONTH, monthOfYear)
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+        updateLabel()
+    }
+
+    private fun updateLabel() {
+        val format = "MM/dd/yyyy"
+        val sdf = SimpleDateFormat(format, Locale.getDefault())
+        binding.dateEt.setText(sdf.format(calendar.time))
     }
 }
